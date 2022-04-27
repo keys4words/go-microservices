11dc5e26d1678a19542f0e4f5ea620afa6b1f8ce
Maxim <keys4words@gmail.com>
2022-04-27 16:25:32 +0300

    Complete mvc structure (#1)

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# go-microservices
+# mvc - REST API on mvc pattern
+1. get users at localhost:8000/users?user_id=1
--- mvc/domain/user_dao.go
+++ mvc/domain/user_dao.go
@@ -1,19 +1,24 @@
 package domain
 
 import (
-	"errors"
 	"fmt"
+	"mvc/utils"
+	"net/http"
 )
 
 var (
 	users = map[int64]*User{
-		1: {Id: 1, FirstName: "Mike", LastName: "Tyson", email: "puncher@mail.com"},
+		1: {Id: 1, FirstName: "Mike", LastName: "Tyson", Email: "puncher@mail.com"},
 	}
 )
 
-func GetUser(userId int64) (User, error) {
-	user := users[userId]
-	if user == nil {
-		return User{}, errors.New(fmt.Sprintf("user %v not found", userId))
+func GetUser(userId int64) (*User, *utils.AppError) {
+	if user := users[userId]; user != nil {
+		return user, nil
+	}
+	return nil, &utils.AppError{
+		Message:    fmt.Sprintf("\nuser %v not found", userId),
+		StatusCode: http.StatusNotFound,
+		Code:       "not_found",
 	}
 }
