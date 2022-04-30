337f1fb0cf9ad183a81134d9c4b3f232994d2eb8
Maxim <keys4words@gmail.com>
2022-05-01 00:33:42 +0300

    add gin-gonic (#4)

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # mvc - REST API on mvc pattern
-1. get users at localhost:8000/users?user_id=1
+1. get users at localhost:8000/users/1
 2. add benchmark test example to utils
 2. add mocking connecting to db via interface to service
+3. play with gin-gonic (add xml/json response)
--- mvc/controllers/users_controller.go
+++ mvc/controllers/users_controller.go
@@ -1,34 +1,31 @@
 package controllers
 
 import (
-	"encoding/json"
 	"mvc/services"
 	"mvc/utils"
 	"net/http"
 	"strconv"
+
+	"github.com/gin-gonic/gin"
 )
 
-func GetUser(res http.ResponseWriter, req *http.Request) {
-	userId, err := strconv.ParseInt(req.URL.Query().Get("user_id"), 10, 64)
+func GetUser(c *gin.Context) {
+	userId, err := strconv.ParseInt(c.Param("user_id"), 10, 64)
 	if err != nil {
 		apiErr := &utils.AppError{
 			Message:    "user_id must be a integer\n",
 			StatusCode: http.StatusBadRequest,
 			Code:       "bad_request",
 		}
-		jsonValue, _ := json.Marshal(apiErr)
-		res.WriteHeader(apiErr.StatusCode)
-		res.Write(jsonValue)
+		utils.RespondError(c, apiErr)
 		return
 	}
 
-	user, apiErr := services.UserService.GetUser(userId)
+	user, apiErr := services.UsersService.GetUser(userId)
 	if apiErr != nil {
-		jsonValue, _ := json.Marshal(apiErr)
-		res.WriteHeader(apiErr.StatusCode)
-		res.Write([]byte(jsonValue))
+		utils.RespondError(c, apiErr)
 		return
 	}
-	jsoValue, _ := json.Marshal(user)
-	res.Write(jsoValue)
+	utils.Respond(c, http.StatusOK, user)
+	c.JSON(http.StatusOK, user)
 }
