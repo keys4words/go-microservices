51c1bd63cf71ff1c3a5ea50bd54a1454e0dc6849
keys4words <keys4words@gmail.com>
2022-05-03 10:01:22 +0300

    finished domain structs

--- src/api/domain/github/create_repo_test.go
+++ src/api/domain/github/create_repo_test.go
@@ -14,11 +14,23 @@ func TestCreateRepoRequestAsJson(t *testing.T) {
 		Homepage:    "https://github.com",
 		Private:     true,
 		HasIssues:   false,
+		HasProjects: false,
 		HasWiki:     false,
 	}
+
 	bytes, err := json.Marshal(request)
 	assert.Nil(t, err)
 	assert.NotNil(t, bytes)
 
 	fmt.Println(string(bytes))
+
+	var target CreateRepoRequest
+	err = json.Unmarshal(bytes, &target)
+	assert.Nil(t, err)
+
+	assert.EqualValues(t, target, request)
+
+	// assert.EqualValues(t,
+	// `{"name":"Golang introduction","description":"my project to work with github api","homepage":"https://github.com","private":true,"has_issues":false,"has_projects":false,"has_wiki":false}`,
+	// string(bytes))
 }
